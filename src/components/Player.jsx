8eb116321d94073d2fe8c013e052637fbe4aec5f
adixadymak <adixadymak@gmail.com>
2024-07-30 15:49:48 +0100

    Adding Challenges to the Demo Project

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,20 +2,17 @@ import { useState, useRef } from "react";
 
 export default function Player() {
   const playerNameRef = useRef();
-  // const [userName, setUserName] = useState(null);
+  const [userName, setUserName] = useState(null);
 
   function handleClick() {
-    // setUserName(playerNameRef.current.value);
+    setUserName(playerNameRef.current.value);
     playerNameRef.current.value = ""; //clearing the input after saving.
   }
 
   // the name inputed does not shoe i.e feedback. The change is for learning purpose.
   return (
     <section id="player">
-      <h2>
-        Welcome{" "}
-        {playerNameRef.current ? playerNameRef.current.value : "unknown entity"}
-      </h2>
+      <h2>Welcome {userName ?? "unknown entity"}</h2>
       <p>
         <input type="text" ref={playerNameRef} />
         <button onClick={handleClick}>Set Name</button>
